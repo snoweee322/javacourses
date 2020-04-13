9d3f5e1eb7ad9d2ab4488aedcc2f80d4f71c299a
snoweee322 <diman199909@gmail.com>
2020-04-13 12:42:52 +0700

    Add exeption and factorial calculation

--- Calculator/src/main/java/ru/sibsutis/Calculator.java
+++ Calculator/src/main/java/ru/sibsutis/Calculator.java
@@ -3,12 +3,33 @@ package ru.sibsutis;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+class MyException extends Exception {
+    public MyException(String message)
+    {
+        super (message);
+    }
+}
+
+class Fact {
+    public static int factorial(Integer n) {
+        try {
+            if (n > 12)
+                throw new MyException("Integer range doesn't include " + n + "!");
+        } catch (MyException e) {
+            System.err.println(e.getMessage());
+            return -1;
+        }
+        if (n.equals(0))
+            return 1;
+        return n * factorial(n-1);
+    }
+}
+
 class Generic { // simple
 
     public <T extends Number> double add (T one, T two) {
         return one.doubleValue() + two.doubleValue();
     }
-
     public <T extends Number> double multiply (T one, T two) {
         return one.doubleValue() * two.doubleValue();
     }
@@ -23,23 +44,23 @@ class Calculator{
         do {
             number1 = getNumber(number1);
             expression = getExpression(expression);
-            number2 = getNumber(number2);
-            Double result;
+            if (!expression.equals('!'))
+                number2 = getNumber(number2);
             switch(expression) {
                 case ('+'):
                     System.out.println("Result = " + gen.add(number1, number2));
                     break;
                 case ('-'):
-                    result = number1 - number2;
-                    System.out.println("Result = " + result);
+                    System.out.println("Result = " + (number1 - number2));
                     break;
                 case ('/'):
-                    result = number1 / number2;
-                    System.out.println("Result = " + result);
+                    System.out.println("Result = " + (number1 / number2));
                     break;
                 case ('*'):
-                    result = number1 * number2;
                     System.out.println("Result = " + gen.multiply(number1, number2));
+                    break;
+                case ('!'):
+                    System.out.println("Result = " + Fact.factorial(number1.intValue()));
                     break;
                 default:
                     System.out.println("Error");
@@ -66,7 +87,7 @@ class Calculator{
             Scanner input = new Scanner(System.in);
             try {
                 expression = input.next().charAt(0);;
-                if(expression.toString().matches("[-+/*]"))
+                if(expression.toString().matches("[-+/*!]"))
                     return expression;
                 else
                     System.out.println("Try again!");
